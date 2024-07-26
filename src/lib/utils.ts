48e9e97c537b406d30289f2fadd3214ba8c4fb51
programmer-ayesha <ayeshagraphical@gmail.com>
2024-07-26 14:52:13 +0500

    apis

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,15 +6,30 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const validateUserId = z.object({
-  userid: z.string()
-});
-export const validateDelete = z.object({
-  userid:z.string(),
-  productid:z.string()
-})
+// export const validateUserId = z.object({
+//   userid: z.string()
+// });
+// export const validateDelete = z.object({
+//   userid:z.string(),
+//   productid:z.string()
+// })
+// export const validatePOST = z.object({
+//   userid:z.string(),
+//   productid:z.string(),
+//   quantity:z.number()
+// })
+
 export const validatePOST = z.object({
-  userid:z.string(),
-  productid:z.string(),
-  quantity:z.number()
-})
+  userid: z.string(),
+  productid: z.string(),
+  quantity: z.number().min(1),
+});
+
+export const validateUserId = z.object({
+  userid: z.string(),
+});
+
+export const validateDelete = z.object({
+  userid: z.string(),
+  productid: z.string(),
+});
